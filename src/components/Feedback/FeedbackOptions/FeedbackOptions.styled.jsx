d28e80403254aed981e88ec1cee3b92f808610d0
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-07-09 12:10:56 +0300

    Add active button

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,15 +1,14 @@
 import styled from "@emotion/styled";
 
 export const Button = styled.button`
-    border-radius: 2px;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     cursor: pointer;
 
-    &:not(:last-child) {
-        margin-right: 10px;
+    &:active {
+        background-color: lightcoral;
     }
 
-    &:hover {
-        background-color: blue;
+    &:not(:last-child) {
+        margin-right: 10px;
     }
 `;
