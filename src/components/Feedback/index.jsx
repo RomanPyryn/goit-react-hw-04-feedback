94ce6b0aace73438c1bd512ef4ba13bd2b3c1d0f
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-07-22 21:55:18 +0300

    Add hooks change

--- src/components/Feedback/index.jsx
+++ src/components/Feedback/index.jsx
@@ -1,45 +1,48 @@
-import { Component } from "react";
+import { useState } from "react";
 import Statistics from "./Statistics";
 import Notification from "./Notification"
 import FeedbackOptions from "./FeedbackOptions";
 import { Container } from "./Feedback.styled"
 
-class Feedback extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    };
+export default function Feedback() {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-    hendleAddFeedback = (e) => {
+    const hendleAddFeedback = (e) => {
         const option = e.currentTarget.name;
-        this.setState({ [option]: this.state[option] + 1 })
+        
+        console.log(good);
+
+        if (option === 'good') {
+            setGood(prevGood => prevGood + 1 )
+        } else if(option === 'neutral'){
+            setNeutral(prevNeutral => prevNeutral + 1 )
+        } else {
+            setBad(prevBad => prevBad + 1 )
+        }
     };
 
-    countTotalFeedback = () => {
-        return this.state.good + this.state.neutral + this.state.bad;
+    const countTotalFeedback = () => {
+        return good + neutral + bad;
     };
 
-    countPositiveFeedbackPercentage = () => {
-        return Math.round(this.state.good / (this.state.good + this.state.neutral + this.state.bad) * 100);
+    const countPositiveFeedbackPercentage = () => {
+        return Math.round(good / (good + neutral + bad) * 100);
     };
 
-    render() {
-      return (
-            <Container>
-                <div>
-                  <h2>Please leave feedback</h2>
-                  
-                  <FeedbackOptions
-                      options={Object.keys(this.state)}
-                      onLeaveFeedback={this.hendleAddFeedback}/>
-                </div>
-                <div>
-                  <h2>Statistic</h2> 
-                  {this.countTotalFeedback() > 0 ? <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/> : <Notification message="There is no feedback"/> }
-                </div>
-            </Container>);
-    };
+    return (
+        <Container>
+            <div>
+              <h2>Please leave feedback</h2>
+              
+              <FeedbackOptions
+                  options={["good", "neutral", "bad"]}
+                  onLeaveFeedback={hendleAddFeedback}/>
+            </div>
+            <div>
+              <h2>Statistic</h2> 
+              {countTotalFeedback() > 0 ? <Statistics good={good} neutral={neutral} bad={bad} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()}/> : <Notification message="There is no feedback"/> }
+            </div>
+        </Container>);
 };
-
-export default Feedback;
